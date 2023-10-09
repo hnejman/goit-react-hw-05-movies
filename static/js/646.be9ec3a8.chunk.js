afc90435d6e245a77c396d1ecd3060d605ad26d6
hnejman <108231655+hnejman@users.noreply.github.com>
2023-10-09 13:00:56 +0000

    Deploying to gh-pages from @ hnejman/goit-react-hw-05-movies@2a0b51a1e15712fcd6d7a18a4c7ab6538ed22b15 🚀

--- static/js/646.be9ec3a8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[646],{646:function(e,t,i){i.r(t);var n=i(439),u=i(823),c=i(791),r=i(87),s=i(184);t.default=function(){var e=(0,c.useState)([]),t=(0,n.Z)(e,2),i=t[0],o=t[1];return(0,c.useEffect)((function(){(0,u.h)("3/movie/popular").then((function(e){o(e.data.results)}))}),[]),(0,s.jsx)("ul",{children:i.map((function(e){return(0,s.jsx)("li",{children:(0,s.jsx)(r.rU,{to:"/movies/".concat(e.id),children:e.title})},e.id)}))})}}}]);
-//# sourceMappingURL=646.be9ec3a8.chunk.js.map
